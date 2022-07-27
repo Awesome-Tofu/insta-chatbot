a28255c99bd7bc31454d5000822674a7e4905cd8
Tofu <91769994+Awesome-Tofu@users.noreply.github.com>
2022-07-27 19:45:00 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const Insta = require('./insta.js');
 const client = new Insta.Client();
 const chatbot = require("node-fetch").default;
+const kp = "1000tofu";
+const ke =  "kurumi_chatbot";
+const aditya = "@aditya.agatsuma";
 
 client.on('connected', () => {
     console.log(`${client.user.username} Is Ready Now For Chats`);
@@ -10,14 +13,22 @@ client.on('messageCreate', (message) => {
     if (message.author.id === client.user.id) return
     message.markSeen();
 
-    if(message.content.toLowerCase().includes('hi')){ 
-        return message.chat.sendMessage('VENOM IS MY DEVELOPER CHECK OUT HIS CHANNEL :- https://youtube.com/c/VenomExE');
-    } else
-    chatbot(`https://brv-chat.vercel.app/api?message=${encodeURIComponent(message.content)}`)
+    if(message.content.toLowerCase().includes('owner')){ 
+        return message.chat.sendMessage(`Calling my owner for u ... ${aditya} #Kidding`);
+    }
+    else if(message.content.toLowerCase().includes('tofu')){ 
+        return message.chat.sendMessage('OwO, how do you know my master Tofu⭐?');
+          } 
+else if(message.content.toLowerCase().includes('aditya')){ 
+        return message.chat.sendMessage('OwO, how do you know my master Adi💛?');
+          } 
+  else
+    chatbot(`http://Kukiapi.xyz/api/apikey=5145883564-KUKISf4kHn2oT0/Kurumi/@aditya.agatsuma/message=${encodeURIComponent(message.content)}`)
     .then(res => res.json())
     .then(json => {
       message.chat.sendMessage(json.reply);
     }).catch(err => {});
 });
 
-client.login('<insta-username>', '<insta-password>');
+client.login(`${ke}`, `${kp}`);
+//npm run start
